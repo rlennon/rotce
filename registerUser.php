889fb7d07c7382fde0e50b8ee7522d4fc94407a9
James Fallon <fallonjames32@gmail.com>
2018-11-11 22:13:56 +0000

    Check for errors and blank fields.Check passwords match

--- registerUser.php
+++ registerUser.php
@@ -20,4 +20,13 @@ if (isset($_POST['reg_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
 ?>
