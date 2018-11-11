a8e63e804b8e498549957d0db2e4a010d6b312c0
James Fallon <fallonjames32@gmail.com>
2018-11-11 22:17:26 +0000

    Register user if no errors, and encrypt password for security

--- registerUser.php
+++ registerUser.php
@@ -45,4 +45,17 @@ if (isset($_POST['reg_user'])) {
     }
   }
 
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (username, email, password)
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+
 ?>
