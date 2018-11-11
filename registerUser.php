21c17ddd2e80983f06081f9c51a5775d144502c8
James Fallon <fallonjames32@gmail.com>
2018-11-11 22:12:32 +0000

    Store user details into variables

--- registerUser.php
+++ registerUser.php
@@ -12,4 +12,12 @@ define('DB_PASSWORD', 'WeWilltakeover!');
 define('DB_DATABASE', 'rotce_minions');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
 ?>
