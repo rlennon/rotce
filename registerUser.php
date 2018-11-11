202fa0b646bd4e762dedfede32f25bb84fcbf8c0
James Fallon <fallonjames32@gmail.com>
2018-11-11 22:15:18 +0000

    Check database to ensure user doest already exist.Display error if it does

--- registerUser.php
+++ registerUser.php
@@ -29,4 +29,20 @@ if (isset($_POST['reg_user'])) {
 	array_push($errors, "The two passwords do not match");
   }
 
+  // first check the database to make sure
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
 ?>
